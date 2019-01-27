9b6f7c009b3bddf4a96df346c8d2d65b8e811da1
webflo <webflo@254778.no-reply.drupal.org>
2019-01-27 13:34:03 +0100

    Issue #3021958 by webflo: Clenaup mail key for the usage in theme suggestions

--- tests/src/Functional/SwiftMailerAlterTest.php
+++ tests/src/Functional/SwiftMailerAlterTest.php
@@ -14,7 +14,13 @@ class SwiftMailerAlterTest extends BrowserTestBase {
 
   public static $modules = ['swiftmailer_test', 'swiftmailer', 'mailsystem'];
 
-  public function testAlter() {
+  /**
+   * @var \Drupal\swiftmailer_test\SwiftMailerDrupalStateLogger
+   */
+  protected $logger = NULL;
+
+  protected function setUp() {
+    parent::setUp();
     Drupal::configFactory()
       ->getEditable('mailsystem.settings')
       ->set('modules.swiftmailer_test.none', [
@@ -26,10 +32,40 @@ class SwiftMailerAlterTest extends BrowserTestBase {
       ->getEditable('swiftmailer.transport')
       ->set('transport', 'null')
       ->save();
+    $this->logger = new SwiftMailerDrupalStateLogger();
+  }
+
+  public function testAlter() {
     \Drupal::state()->set('swiftmailer_test_swiftmailer_alter_1', TRUE);
     \Drupal::service('plugin.manager.mail')->mail('swiftmailer_test', 'test_1', 'test@example.com', \Drupal::languageManager()->getDefaultLanguage()->getId());
-    $logger = new SwiftMailerDrupalStateLogger();
-    $this->assertEquals('Replace text in swiftmailer_test_swiftmailer_alter', $logger->dump()[0]['body']);
+    $this->assertEquals('Replace text in swiftmailer_test_swiftmailer_alter', $this->logger->dump()[0]['body']);
+  }
+
+  public function testTemplatePreprocess() {
+    \Drupal::configFactory()
+      ->getEditable('swiftmailer.message')
+      ->set('respect_format', FALSE)
+      ->save();
+
+    \Drupal::configFactory()
+      ->getEditable('system.theme')
+      ->set('default', 'swiftmailer_test_theme')
+      ->save();
+
+    \Drupal::configFactory()
+      ->getEditable('mailsystem.settings')
+      ->set('theme', 'default')
+      ->save();
+
+    \Drupal::service('theme_installer')->install(['swiftmailer_test_theme']);
+
+    $params = [
+      'format' => SWIFTMAILER_FORMAT_HTML,
+    ];
+
+    \Drupal::service('plugin.manager.mail')->mail('swiftmailer_test', 'test-2', 'test@example.com', \Drupal::languageManager()->getDefaultLanguage()->getId(), $params);
+    $this->assertContains('string_from_template', (string) $this->logger->dump()[0]['body']);
+    $this->assertContains('variable_from_preprocess', (string) $this->logger->dump()[0]['body']);
   }
 
 }
