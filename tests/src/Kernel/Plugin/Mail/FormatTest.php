9c854d13f14dbd471282a4905a0a90e8620f1fc6
AdamPS <AdamPS@2650563.no-reply.drupal.org>
2020-04-04 16:37:43 +0100

    Issue #3124110 by AdamPS: Fix/improve the tests for SwiftMailer::format (step 2)

--- tests/src/Kernel/Plugin/Mail/FormatTest.php
+++ tests/src/Kernel/Plugin/Mail/FormatTest.php
@@ -19,6 +19,7 @@ class FormatTest extends KernelTestBase {
     'user',
     'filter',
     'swiftmailer',
+    'system',
   ];
 
   /**
@@ -41,6 +42,13 @@ class FormatTest extends KernelTestBase {
     $this->installSchema('user', 'users_data');
     $this->plugin = $this->container->get('plugin.manager.mail')
       ->createInstance('swiftmailer');
+
+    // Install the test theme for a simple template.
+    \Drupal::service('theme_installer')->install(['swiftmailer_test_theme']);
+    \Drupal::configFactory()
+      ->getEditable('system.theme')
+      ->set('default', 'swiftmailer_test_theme')
+      ->save();
   }
 
   /**
@@ -55,12 +63,14 @@ class FormatTest extends KernelTestBase {
 
     $message['params']['format'] = SWIFTMAILER_FORMAT_HTML;
     $actual = $this->plugin->format($message);
-    $this->assertSame(is_array($expected) ? implode(PHP_EOL, $expected) : $expected, (string) $actual['body']);
+    $expected = implode(PHP_EOL, $expected) . PHP_EOL;
+    $this->assertSame($expected, (string) $actual['body']);
 
     if ($expected_plain) {
       $message['params']['format'] = SWIFTMAILER_FORMAT_PLAIN;
       $actual = $this->plugin->massageMessageBody($message);
-      $this->assertSame(is_array($expected_plain) ? implode(PHP_EOL, $expected_plain) : $expected_plain, (string) $actual['body']);
+      $expected_plain = implode(PHP_EOL, $expected_plain);
+      $this->assertSame($expected_plain, (string) $actual['body']);
     }
   }
 
@@ -72,34 +82,49 @@ class FormatTest extends KernelTestBase {
       'with html' => [
         'message' => [
           'body' => [
-            Markup::create('<p>Lorem ipsum dolor sit amet</p>'),
-            Markup::create('<p>consetetur sadipscing elitr</p>'),
-            Markup::create('<p>sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>'),
-            Markup::create('<p>sed diam voluptua.</p>'),
+            Markup::create('<p>Lorem ipsum &amp; dolor sit amet</p>'),
+            Markup::create('<p>consetetur &lt; sadipscing elitr</p>'),
           ],
         ],
-        'expected' => "<p>Lorem ipsum dolor sit amet</p>\n<p>consetetur sadipscing elitr</p>\n<p>sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>\n<p>sed diam voluptua.</p>",
+        'expected' => [
+          "<p>Lorem ipsum &amp; dolor sit amet</p>",
+          "<p>consetetur &lt; sadipscing elitr</p>",
+        ],
+        'expected_plain' => [
+          "<p>Lorem ipsum &amp; dolor sit amet</p>",
+          "<p>consetetur &lt; sadipscing elitr</p>",
+        ],
       ],
       'no html' => [
         'message' => [
           'body' => [
-            "Lorem ipsum dolor sit amet\nconsetetur sadipscing elitr\nsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat\nsed diam voluptua.",
+            "Lorem ipsum & dolor sit amet\nconsetetur < sadipscing elitr",
           ],
         ],
-        'expected' => "<p>Lorem ipsum dolor sit amet<br />\nconsetetur sadipscing elitr<br />\nsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat<br />\nsed diam voluptua.</p>\n",
+        'expected' => ["<p>Lorem ipsum &amp; dolor sit amet<br />\nconsetetur &lt; sadipscing elitr</p>\n"],
+        'expected_plain' => ["Lorem ipsum & dolor sit amet\nconsetetur < sadipscing elitr\n"],
       ],
       'mixed' => [
         'message' => [
           'body' => [
-            'Hello World',
-            'Hello <strong>World</strong>',
-            new FormattableMarkup('Hello World #@number', ['@number' => 2]),
-            Markup::create('Hello <strong>World</strong>'),
+            'Hello & World',
+            'Hello & <strong>World</strong>',
+            new FormattableMarkup('Hello &amp; World #@number', ['@number' => 2]),
+            Markup::create('Hello &amp; <strong>World</strong>'),
           ],
         ],
         // Output is wrong due to https://www.drupal.org/project/swiftmailer/issues/3122389.
-        'expected' => ["<p>Hello World</p>\n", "Hello *World*\n", "<p>Hello World #2</p>\n", "Hello <strong>World</strong>"],
-        'expected_plain' => ["Hello World\n", "Hello *World*\n", "Hello World #2", "Hello <strong>World</strong>"],
+        'expected' => [
+          "<p>Hello &amp; World</p>\n",
+          "Hello & *World*\n",
+          "<p>Hello &amp;amp; World #2</p>\n", "Hello &amp; <strong>World</strong>",
+        ],
+        'expected_plain' => [
+          "Hello & World\n",
+          "Hello & *World*\n",
+          "Hello &amp; World #2",
+          "Hello &amp; <strong>World</strong>",
+        ],
       ],
     ];
   }
